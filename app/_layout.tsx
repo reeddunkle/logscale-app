f6d8ae3f481275fb48daf3bf2f63b67476496545
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2025-01-18 21:21:04 -0500

    wire up drizzle

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,15 +3,22 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { SQLiteProvider, openDatabaseSync } from "expo-sqlite";
 import { Suspense, useEffect } from "react";
-import "react-native-reanimated";
+import { ActivityIndicator } from "react-native";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { ActivityIndicator } from "react-native";
+import migrations from "@/drizzle/migrations";
+
+import "react-native-reanimated";
+
+const DATABASE_NAME = "logs";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -21,6 +28,10 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  const expoDb = openDatabaseSync(DATABASE_NAME);
+  const db = drizzle(expoDb);
+  const { success, error } = useMigrations(db, migrations);
 
   useEffect(() => {
     if (loaded) {
@@ -35,10 +46,16 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Suspense fallback={<ActivityIndicator size="large" />}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
+        <SQLiteProvider
+          databaseName={DATABASE_NAME}
+          options={{ enableChangeListener: true }}
+          useSuspense
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </SQLiteProvider>
       </Suspense>
       <StatusBar style="auto" />
     </ThemeProvider>
